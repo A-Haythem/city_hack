73a357a5ae4a635c3fb1da46a10b28a42f413c75
Ajlani Haythem <52455130+KeLLaNNNN@users.noreply.github.com>
2020-07-18 12:14:41 +0100

    update

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,21 @@
 const router = require('express').Router();
 const User = require('../models/User');
+//validation
+const Joi = require('@hapi/joi');
+const schema = Joi.object({
+    username: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+});
+
 router.post('/register', async (req,res)=>{
-    const user= new User({
+    const validation = schema.validate(req.body);
+    res.send(validation);
+    const user = new User({
         username: req.body.username,
         email: req.body.email,
         password: req.body.password,
-    })
+    });
     try{
         const savedUser = await user.save();
         res.send(savedUser);
