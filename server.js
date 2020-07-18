fadc719fbe1a3016d529d50c05b6272f4cc7085f
firas bouali <firas.bouali11@gmail.com>
2020-07-18 11:34:14 +0100

    post

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express()
 const port = 1234
 const mongoose = require("mongoose")
 
-// hpTrDOPgqpw2rZ09
 mongoose.connect("mongodb+srv://laclass:laclass11213@cluster0.qdfum.mongodb.net/root?retryWrites=true&w=majority",{
     useUnifiedTopology: true,
     useNewUrlParser: true
