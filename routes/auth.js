a0593a00d68a8c1f719de0353672a8d6ceea3081
firas bouali <firas.bouali11@gmail.com>
2020-07-18 16:11:08 +0100

    firas

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
-const{registrationValidation,loginValidation} = require('../validation')
+const{registrationValidation,loginValidation} = require('../middleware/validation')
 
 router.post('/register', async (req,res)=>{
     const {password,password2,email,username}= req.body
